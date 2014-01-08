c81b990fd51de7491e8ccb213b1b7c4872305e54
7korobi <7korobi@gmail.com>
2014-01-09 00:48:05 +0900

    link structure fix.

--- init/copy.sh
+++ init/copy.sh
@@ -6,8 +6,12 @@ copy_out () {
   cp  $1   /mnt/hgfs/set/${UTAGE}$1
 }
 
-copy_out /etc/ssh/sshd_config
+
+copy_out /etc/fstab
+copy_out /etc/passwd
+copy_out /etc/group
 copy_out /etc/hosts
+copy_out /etc/ssh/sshd_config
 copy_out /etc/network/interfaces 
 copy_out /etc/sysconfig/network-scripts/ifcfg-eth0
 copy_out /root/.ssh/authorized_keys
